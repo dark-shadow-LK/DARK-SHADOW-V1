47d8dab4958e9401ffc64d49c015e95be954493f
Dark Shadow <155859542+dark-shadow-LK@users.noreply.github.com>
2024-10-08 23:15:23 +0530

    👨‍💻DARK-SHADOW👨‍💻
    👨‍💻DARK-SHADOW👨‍💻

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KLIgFTqB#gPVxxjiIyPHBuzBZV9twK4FE7vwQhFCoVGSTHOa1M_o",
+SESSION_ID: process.env.SESSION_ID || "enter your session id",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/1ece2e0281513c05d20ee.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🎉 Hi DARK-SHADOW-MD Is Online Now 💻\n*💻 Owner* - DARK-SHADOW-MD\n\n*💻 Owner Number* -94773416478",
